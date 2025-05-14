ea592c1d1744abd58906e37fc6af65fd1bd5d1ae
Kenpachi7Zaraki <arvee389@gmail.com>
2025-05-14 20:12:30 +0530

    Add files via upload
    College project. Using Web development tools!

---
+++ data.js
@@ -0,0 +1,52 @@
+ let movies = [
+    {
+        name: 'Loki',
+        des: 'Loki is a TV series that premiered on June 9, 2021, and concluded on July 14, 2023. The first season, consisting of six episodes, received positive reviews for its performances, musical score, and Loki character arc.2 In season 1, Loki connection with Mobius, Sylvie, and his experience of the TVA gives him a second chance to rethink and redefine what purpose might mean. Through the conversation with Mobius, the hubris of his entitlement of that glorious purpose is revealed to be a fallacy devoid of meaning.1 The second season, also consisting of six episodes, ran from October 5 to November 9, 2023, as part of Phase Five of the MCU.2 Disney released Loki Season 1 and other series for the first time in 4K UHD in steelbook.',
+        image: 'images/poster20.png'
+    },
+    {
+        name: 'Wanda-Vision',
+        des: 'WandaVision is an American television miniseries created by Jac Schaeffer for the streaming service Disney+, based on Marvel Comics featuring the characters Wanda Maximoff / Scarlet Witch and Vision. It is the first television series in the Marvel Cinematic Universe (MCU) produced by Marvel...',
+        image: 'images/poster2.png'
+    },
+    {
+        name: 'Raya And The Last Dragon',
+        des: 'Raya and the Last Dragon (/ˈraɪ.ə/ RYE-ə) is a 2021 American animated fantasy action-adventure film produced by Walt Disney Animation Studios and distributed by Walt Disney Studios Motion Pictures. It was directed by Don Hall and Carlos López Estrada, co-directed by Paul Briggs and John... ',
+        image: 'images/poster4.png'
+    },
+    {
+        name: 'THOR',
+        des: 'Thor Fight with the Giants (Tors strid med jättarna) by Mårten Eskil Winge (1872). Thor (from Old Norse: Þórr) is a prominent god in Germanic paganism. In Norse mythology, he is a hammer-wielding god associated with lightning, thunder, storms, sacred groves and trees, strength, the protection.. ',
+        image: 'images/poster17.png'
+    },
+    {
+        name: 'JOKER',
+        des: 'Joker is a 2021 American animated coming-of-age fantasy film produced by Walt Disney Pictures and Pixar Animation Studios and distributed by Walt Disney Studios Motion Pictures. The film was directed by Enrico Casarosa in his feature directorial debut, produced by Andrea Warren and written by..',
+        image: 'images/poster22.png'
+    },
+    {
+        name: 'Spiderman',
+        des: 'Spider-Man is a superhero appearing in American comic books published by Marvel Comics. Created by writer-editor Stan Lee and artist Steve Ditko, he first appeared in the anthology comic book Amazing Fantasy #15 (August 1962) in the Silver Age of Comic Books...',
+        image: 'images/poster6.png'
+    },
+    {
+        name: 'AVENGERS',
+        des: 'Marvel The Avengers (classified under the name Marvel Avengers Assemble in the United Kingdom and Ireland), or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name. Produced by Marvel Studios and distributed by Walt Disney Studios.... ',
+        image: 'images/poster3.png'
+    },
+    {
+        name: 'Monet Heist',
+        des: 'Money Heist (Spanish: La casa de papel, [la ˈkasa ðe paˈpel], lit. The House of Paper is a Spanish heist crime drama television series created by Álex Pina. The series traces two long-prepared heists led by the Professor (Álvaro Morte), one on the Royal Mint of Spain, and one on the .. ',
+        image: 'images/poster11.png'
+    },
+    {
+        name: 'Strangers Things',
+        des: 'Stranger Things is an American science fiction horror drama television series created by the Duffer Brothers for Netflix. Produced by Monkey Massacre Productions and 21 Laps Entertainment, the first season was released on Netflix on July 15, 2016. The second and third season followed in October... ',
+        image: 'images/poster13.png'
+    },
+    {
+        name: 'Minions',
+        des: 'Minions is a 2015 American animated comedy film produced by Illumination Entertainment and distributed by Universal Pictures. The film is a spin-off/prequel to Despicable Me (2010) and the third installment overall in the film series of the same name. Directed by Pierre Coffin and Kyle Balda..... ',
+        image: 'images/poster15.png'
+    }
+]
